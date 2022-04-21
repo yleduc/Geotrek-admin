09d2b8280cf8239fba1604c07fcccfd8c245bad7
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2022-04-21 18:15:01 +0200

    Add egg in setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     scripts=['manage.py'],
     install_requires=[
         'Django==3.1.*',
-        'mapentity @ git+https://github.com/makinacorpus/django-mapentity.git@master',
+        'mapentity @ git+https://github.com/makinacorpus/django-mapentity.git@master#egg=mapentity',
         'env_file',
         # pinned by requirements.txt
         'python-memcached',
