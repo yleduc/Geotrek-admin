22e2a8b7f565c486121e45e083a816c5d23631db
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2022-05-04 18:07:01 +0200

    Add test add file with other structure

--- geotrek/authent/tests/test_structures.py
+++ geotrek/authent/tests/test_structures.py
@@ -2,9 +2,12 @@
     Unit tests
 """
 from .base import AuthentFixturesTest
+from django.urls import reverse
 
 from geotrek.authent.tests.factories import StructureFactory, PathManagerFactory
 from geotrek.core.tests.factories import PathFactory
+from geotrek.common.tests.factories import AttachmentFactory
+from geotrek.common.utils.testdata import get_dummy_uploaded_image
 
 
 class StructureTest(AuthentFixturesTest):
@@ -27,4 +30,29 @@ class StructureTest(AuthentFixturesTest):
         p.save()
         self.assertNotEqual(p.structure, user.profile.structure)
         response = self.client.get(p.get_update_url())
+        self.assertEqual(response.status_code, 302)
+
+    def test_structure_restricted_attachment(self):
+        """Attachment should not be modified"""
+        p = PathFactory()
+        picture = AttachmentFactory(content_object=p, title='img1',
+                                    attachment_file=get_dummy_uploaded_image())
+        # Login
+        user = PathManagerFactory(password="foo")
+        self.client.login(username=user.username, password="foo")
+
+        # Add an attachment to path from same structure
+        update_attachment_url = reverse(
+            'update_attachment',
+            kwargs={'attachment_pk': picture.pk}
+        )
+        response = self.client.get(update_attachment_url)
+        self.assertEqual(response.status_code, 200)
+
+        # Add attachment or remove existing one from other structure
+        p.structure = StructureFactory(name="Other")
+        p.save()
+        self.assertNotEqual(p.structure, user.profile.structure)
+
+        response = self.client.get(update_attachment_url)
         self.assertEqual(response.status_code, 302)
